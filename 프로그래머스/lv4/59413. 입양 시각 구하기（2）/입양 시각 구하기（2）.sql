9772dedc21c990b9911002a9db38d10a14e6874b
sooyun <55366664+imcodding@users.noreply.github.com>
2023-05-21 17:24:19 +0900

    Update 입양 시각 구하기（2）.sql
    다른 풀이 방식 추가

--- 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,3 +1,4 @@
+# 나의 풀이
 SELECT
     hours.hour,
     SUM(IF(ao.animal_id IS NULL, 0, 1)) AS `count`
@@ -11,3 +12,17 @@ RIGHT JOIN
 ON hour(ao.datetime) = hours.hour
 GROUP BY hours.hour;
 
+# 다른 풀이 - WITH RECURSIVE 구문
+# 가상 테이블을 생성하면서 가상 테이블 자신의 값을 참조하여 값을 결정할 때 사용
+WITH RECURSIVE cte AS (
+    SELECT 0 as n
+    UNION ALL
+    SELECT n + 1 FROM cte WHERE n < 23
+)
+SELECT 
+    cte.n AS hour, 
+    COUNT(a.animal_id) AS `count`
+FROM cte
+LEFT JOIN animal_outs a ON cte.n = HOUR(a.datetime)
+GROUP BY cte.n
+ORDER BY cte.n;
