64219755bc0497d141bdc933e5f1c7a6e486299b
sooyun <55366664+imcodding@users.noreply.github.com>
2024-04-12 14:05:53 +0900

    Update 그룹별 조건에 맞는 식당 목록 출력하기.sql

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,20 +1,19 @@
-WITH sum_review AS (
-    SELECT 
-        member_id,
-        SUM(review_score) AS review_score
-    FROM rest_review
-    GROUP BY member_id, review_date
-), reviewer_ids AS (
-    SELECT member_id
-    FROM sum_review
-    WHERE review_score = (SELECT MAX(review_score) FROM sum_review)
+WITH review_cnt AS (
+	SELECT
+		member_id,
+		count(member_id) AS cnt
+	FROM rest_review rr 
+	GROUP BY member_id
+), reviewer_max AS (
+	SELECT member_id 
+	FROM review_cnt 
+	WHERE cnt = (SELECT max(cnt) FROM review_cnt)
 )
 SELECT
-    mem.member_name,
-    rv.review_text,
-    date_format(rv.review_date, '%Y-%m-%d')
-FROM member_profile mem
-INNER JOIN rest_review rv
-ON mem.member_id = rv.member_id
-WHERE mem.member_id IN (SELECT member_id FROM reviewer_ids)
-ORDER BY rv.review_date, rv.review_text
+	member_name,
+	review_text,
+	date_format(review_date, '%Y-%m-%d') AS review_date
+FROM member_profile m, rest_review rr
+WHERE m.member_id = rr.member_id
+AND rr.member_id IN (SELECT member_id FROM reviewer_max)
+ORDER BY review_date, review_text
